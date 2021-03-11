dfe53acb5083fd3ecbf1426e9687d49f0efe4a21
Anil_Ramcharran <anil.gitme@gmail.com>
2021-03-10 23:55:46 -0500

    finished need to test

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ const server = express();
 // })
 
 server.get('/api/users/:id', (req, res) => {
-    const id = req.params;
+    const id = req.params.id;
     users.find(id)
         .then(user => {
             console.log(user);
@@ -21,7 +21,7 @@ server.get('/api/users/:id', (req, res) => {
             }
         })
         .catch(error => {
-            res.status(500).json({ message: 'The user information could not be retrieved', error })
+            res.status(500).json({ error: err.message });
         })
 })
 
@@ -32,11 +32,54 @@ server.get('api/users', (req, res) => {
             res.status(200).json(user)
         })
         .catch(error => {
-            res.status(500).json({ message: 'The user information could not be retrieved', error })
+            res.status(500).json({ error: err.message });
         })
 })
 
-server.post('')
+server.post('/api/users', (req, res) => {
+    if (!req.body.name || !req.body.bio) {
+        res.status(400).json({ message: 'Please provide name and bio for the user"' })
+    } else {
+        users.insert({ name: req.body.name, bio: req.body.bio })
+            .then((res) => {
+                res.status(201).json(res);
+            })
+            .catch(error => {
+                res.status(500).json({ error: err.message });
+            })
+    }
+})
+
+server.put('/api/users/:id', async(req, res) => {
+    const { id } = req.params
+    try {
+        if (!req.body.name || !req.body.bio) {
+            res.status(400).json({ message: 'Please provide name and bio for the user' })
+        } else {
+            const updateUser = await users.update(id, req.body)
+            if (!updateUser) {
+                res.status(404).json({ message: 'The user with the specified ID does not exist' })
+            } else {
+                res.json(updateUser)
+            }
+        }
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+})
+
+server.delete('/api/users/:id', async(req, res) => {
+    try {
+        const deleteUser = await users.remove(req.params.id)
+        if (!deleteUser) {
+            res.status(404).json({ message: 'The user with the specified ID does not exist' })
+        } else {
+            res.json(deleteUser)
+        }
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+})
 
 
 
